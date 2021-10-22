cb5c7abada5846e7ad4de560db1d2fc6328bd66f
Gheorghe Coanta <gheorghe.coanta@gmail.com>
2021-10-22 20:59:56 +0300

    AlgoritmParcurgereGenerica

---
+++ src/Graph.java
@@ -0,0 +1,109 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class Graph
+{
+
+    private List<Node> nodeList = new ArrayList<>();
+
+    public Graph() {
+    }
+
+    public Graph(List<Node> nodeList)
+    {
+        this.nodeList = nodeList;
+    }
+
+    public void addNode(Node node)
+    {
+        nodeList.add(node);
+
+    }
+
+    public List<Integer> getLinks(Integer node)
+    {
+        for (int i = 0; i <nodeList.size(); i++)
+        {
+            if (node== nodeList.get(i).getNodeNumber())
+            {
+             return nodeList.get(i).getLinks();
+            }
+        }
+    return null;
+    }
+
+    public void printGraph()
+    {
+        for (Node node:nodeList)
+        {
+            System.out.print(node.getNodeNumber()+": ");
+            System.out.println(node.getLinks());
+            System.out.println("-------------------");
+        }
+    }
+
+    public void parcurgereGenerala(Integer startNode)
+    {
+        List<Integer> unvizited = new ArrayList<>();
+        List<Integer> p = new ArrayList<>();
+        List<Integer> o = new ArrayList<>();
+        for (Node node:nodeList)
+        {
+            unvizited.add(node.getNodeNumber());
+            p.add(0);
+            o.add(-1);
+        }
+        unvizited.remove(startNode);
+    //    System.out.println(unvizited);
+
+        List<Integer> vizited = new ArrayList<>();
+        vizited.add(startNode);
+      //  System.out.println(vizited);
+        List<Integer> analized = new ArrayList<>();
+        System.out.println(p);
+
+        Integer k;
+        k=1; o.set(startNode-1,1);
+
+        Integer selectedNode;
+        while (!vizited.isEmpty())
+        {
+            selectedNode = vizited.get(0);
+
+            List<Integer> selectedNodeLinks= getLinks(selectedNode);
+
+            if (!selectedNodeLinks.isEmpty())
+            {
+                k++;
+                for (int i = 0; i < selectedNodeLinks.size(); i++)
+                {
+                    if (unvizited.contains(selectedNodeLinks.get(i)))
+                    {
+                    unvizited.remove(selectedNodeLinks.get(i));
+                    vizited.add(selectedNodeLinks.get(i));
+                    p.set(selectedNodeLinks.get(i)-1,selectedNode);
+                    o.set(selectedNodeLinks.get(i)-1,k);
+                    }
+
+                }
+            } else
+            {
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+
+}
---
+++ src/Node.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node
+{
+    private Integer nodeNumber;
+    private List<Integer> links = new ArrayList<>();
+
+    public Node()
+    {
+    }
+
+    public Node(Integer nodeNumber, List<Integer> links)
+    {
+        this.nodeNumber = nodeNumber;
+        this.links = links;
+    }
+
+    public Integer getNodeNumber()
+    {
+        return nodeNumber;
+    }
+
+    public Node setNodeNumber(Integer nodeNumber) {
+        this.nodeNumber = nodeNumber;
+        return this;
+    }
+
+    public List<Integer> getLinks() {
+        return links;
+    }
+
+    public Node setLinks(List<Integer> links) {
+        this.links = links;
+        return this;
+    }
+}
